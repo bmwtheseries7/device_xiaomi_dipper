0b7049bd3fe984f0d1c3b41a677ce5f9f805fbf6
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2025-02-14 08:30:11 +0700

    dipper: Adapt to PixelOS
    Change-Id: Id14ac279fc3ed47ee34e81ff2f4099c3e712c69c

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-custom
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
