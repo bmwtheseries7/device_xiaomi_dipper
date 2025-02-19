LineageOS/android_device_xiaomi_dipper

dipper: Add device specific init script
Set stock current draw for ir & notification leds

Change-Id: I699df5cbe70b6fe93ee0ae1a4ac17b3d329c1a08

rootdir/Android.mk
@@ -7,3 +7,11 @@ LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := etc/fstab.qcom
 LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.device.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.device.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)

bmwtheseries7/device_xiaomi_dipper

 * Combined with init.brcm.rc and init.remosaic.rc

rootdir/Android.mk
@@ -7,3 +7,11 @@ LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := etc/fstab.qcom
 LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.target.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.target.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)